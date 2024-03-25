e919ea675d16b3dc9d2d48562632c141c5caed86
Paul OGGERO <paul.oggero@itgrate.com>
2024-03-25 17:51:32 +0700

    🎉 Import base files

---
+++ response.dart
@@ -0,0 +1,68 @@
+class ApiResponse<T> {
+  final List<T> hits;
+  final List<dynamic> facetCounts;
+  final int found;
+
+  ApiResponse({
+    required this.hits,
+    required this.facetCounts,
+    required this.found,
+  });
+
+  factory ApiResponse.fromJson(
+      Map<String, dynamic> json, T Function(Map<String, dynamic>) fromJson) {
+    return ApiResponse<T>(
+      hits: (json['hits'] as List<dynamic>)
+          .map((hit) => fromJson(hit['document'] as Map<String, dynamic>))
+          .toList(),
+      facetCounts: json['facet_counts'] as List<dynamic>,
+      found: json['found'] as int,
+    );
+  }
+}
+
+class ApiGroupedResponse<T> {
+  final List<ApiGroup<T>> groupedHits;
+  final List<dynamic> facetCounts;
+  final int foundDocs;
+  final int found;
+
+  ApiGroupedResponse(
+      {required this.groupedHits,
+      required this.facetCounts,
+      required this.foundDocs,
+      required this.found});
+
+  factory ApiGroupedResponse.fromJson(
+      Map<String, dynamic> json, T Function(Map<String, dynamic>) fromJson) {
+    final List<dynamic> groupedHitsJson = json['grouped_hits'] as List<dynamic>;
+
+    return ApiGroupedResponse<T>(
+      groupedHits: groupedHitsJson
+          .map((hitJson) => ApiGroup.fromJson(hitJson, fromJson))
+          .toList(),
+      facetCounts: json['facet_counts'] as List<dynamic>,
+      foundDocs: json['found_docs'] as int,
+      found: json['found'] as int,
+    );
+  }
+}
+
+class ApiGroup<T> {
+  final int found;
+  final List<dynamic> groupKeys;
+  final List<T> hits;
+
+  ApiGroup({required this.found, required this.groupKeys, required this.hits});
+
+  factory ApiGroup.fromJson(
+      Map<String, dynamic> json, T Function(Map<String, dynamic>) fromJson) {
+    return ApiGroup<T>(
+      hits: (json['hits'] as List<dynamic>)
+          .map((hit) => fromJson(hit['document'] as Map<String, dynamic>))
+          .toList(),
+      groupKeys: json['group_key'] as List<dynamic>,
+      found: json['found'] as int,
+    );
+  }
+}
---
+++ search_service.dart
@@ -0,0 +1,106 @@
+import 'dart:io';
+
+import 'package:cocktail_app/application/services/search/response.dart';
+import 'package:typesense/typesense.dart';
+
+class SearchService {
+  final Client client;
+  List<Schema> collections = [];
+
+  SearchService._(this.client);
+
+  factory SearchService(
+      {required String apiKey,
+      required InternetAddress host,
+      String scheme = 'http',
+      int? port = 8108,
+      Protocol protocol = Protocol.http,
+      int retries = 3,
+      int timeout = 3}) {
+    final configuration = Configuration(apiKey,
+        nodes: {
+          Node.withUri(Uri(scheme: scheme, host: host.address, port: port))
+        },
+        numRetries: retries,
+        connectionTimeout: Duration(seconds: timeout));
+
+    return SearchService._(Client(configuration));
+  }
+
+  factory SearchService.withHostAddress(
+      {required String apiKey,
+      required String hostAdress,
+      InternetAddressType hostAdressType = InternetAddressType.unix,
+      String scheme = 'http',
+      int? port = 8108,
+      Protocol protocol = Protocol.http,
+      int retries = 3,
+      int timeout = 3}) {
+    return SearchService(
+        apiKey: apiKey,
+        host: InternetAddress(hostAdress, type: hostAdressType),
+        scheme: scheme,
+        port: port,
+        protocol: protocol,
+        retries: retries,
+        timeout: timeout);
+  }
+
+  Future<dynamic> search<T>(String search, String collectionName,
+      {required String queryBy,
+      String? filterBy,
+      String? sortBy,
+      String? groupBy,
+      int? groupLimit,
+      int? page,
+      int? limit,
+      int? offset,
+      required T Function(Map<String, dynamic>) fromJson}) async {
+    final searchParameters = {'q': '$search', 'query_by': '$queryBy'};
+
+    if (filterBy != null) {
+      searchParameters['filter_by'] = '$filterBy';
+    }
+
+    if (sortBy != null) {
+      searchParameters['sort_by'] = '$sortBy';
+    }
+
+    if (groupBy != null) {
+      searchParameters['group_by'] = '$groupBy';
+      if (groupLimit != null) {
+        searchParameters['group_limit'] = '$groupLimit';
+      }
+    }
+
+    if (page != null) {
+      searchParameters['page'] = '$page';
+    }
+
+    if (limit != null) {
+      searchParameters['limit'] = '$limit';
+    }
+
+    if (offset != null) {
+      searchParameters['offset'] = '$offset';
+    }
+
+    print(
+        "search on collection $collectionName with parameters $searchParameters");
+
+    try {
+      final Map<String, dynamic> result = await client
+          .collection(collectionName)
+          .documents
+          .search(searchParameters);
+
+      if (groupBy == null) {
+        return ApiResponse.fromJson(result, fromJson);
+      } else {
+        return ApiGroupedResponse.fromJson(result, fromJson);
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+}
