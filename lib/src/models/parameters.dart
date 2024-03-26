5cc172444724464c317fc67e27e14679c315f849
Paul OGGERO <paul.oggero@itgrate.com>
2024-03-26 10:11:07 +0700

    🎨 Add error handling in parameters construct to ensure good parametemeters

--- lib/src/models/parameters.dart
+++ lib/src/models/parameters.dart
@@ -20,7 +20,33 @@ class Parameters {
       this.groupLimit,
       this.page,
       this.limit,
-      this.offset});
+      this.offset}) {
+    if (queryBy.isEmpty) {
+      throw ArgumentError('Ensure Parameters.queryBy is not empty');
+    }
+
+    if (page != null && page! < 0) {
+      throw ArgumentError('Ensure Parameters.page is not a negative number');
+    }
+
+    if (groupLimit != null && groupLimit! < 0) {
+      throw ArgumentError(
+          'Ensure Parameters.groupLimit is not a negative number');
+    }
+
+    if (limit != null && limit! < 0) {
+      throw ArgumentError('Ensure Parameters.limit is not a negative number');
+    }
+
+    if (offset != null && offset! < 0) {
+      throw ArgumentError('Ensure Parameters.offset is not a negative number');
+    }
+
+    if (groupLimit != null && groupBy == null) {
+      throw ArgumentError(
+          'Ensure Parameters.groupBy is not a null if Parameters.groupLimit is not null');
+    }
+  }
 
   Map<String, dynamic> toMap() {
     final Map<String, dynamic> searchParameters = {
